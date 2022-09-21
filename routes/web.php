b8f965e68f5da6828752fb22ad2e853ab19e45da
d.rogovaya <rogovaya_d@iac.spb.ru>
2022-09-21 12:26:04 +0300

    lesson11_1(middleware correct)

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('profile', function () {
 
 Route::get('/{locale}/address', [AddressController::class,'show'])
     ->name('address.show')
-    ->middleware('SetLocale');
+    ->middleware('setLocale');
 
 Auth::routes();
 
